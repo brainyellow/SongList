6478069d47812713288a7e0c4b0f35c26275505d
Tyler Latawiec <tylerwithpie@aol.com>
2018-09-25 23:21:40 -0400

    duplicate checks

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,13 +4,11 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,3 +1,5 @@
+// Tyler Latawiec
+// Brian Huang
 package application;
 
 import javafx.scene.Scene;
@@ -8,55 +10,49 @@ import com.google.gson.Gson;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 
+public class MainController {
+    @FXML
+    private TextField titleField;
+    @FXML
+    private TextField artistField;
+    @FXML
+    private TextField yearField;
+    @FXML
+    private TextField albumField;
 
-public class MainController{
-    @FXML public TextField titleField;
-    @FXML public TextField artistField;
-    @FXML public TextField yearField;
-    @FXML public TextField albumField;
+    @FXML
+    private TableView<Song> songTable;
+    @FXML
+    private TableColumn<Song, String> colTitle;
+    @FXML
+    private TableColumn<Song, String> colArtist;
 
-    @FXML TableView<Song> songTable;
-    @FXML TableColumn<Song, String> colTitle;
-    @FXML TableColumn<Song, String> colArtist;
-    @FXML TableColumn<Song, String> colYear;
-    @FXML TableColumn<Song, String> colAlbum;
+    @FXML
+    private Rectangle topBar;
+
     
-    @FXML Rectangle topBar;
+    // List of songs
+    private ObservableList<Song> songs = FXCollections.observableArrayList();
 
-    @FXML Scene scene;
-
+    //Used for adding songs/checking for dupicates
     private Song current;
-    private Song songHolder;
-    private String title, artist, album, year;
 
-    ObservableList<Song> songs = FXCollections.observableArrayList();
 
     @FXML
     public void initialize() {
-    	
-    	// setting correct value types of the columns
-        colTitle.setCellValueFactory(
-            new PropertyValueFactory<Song,String>("title")
-        );
-        colArtist.setCellValueFactory(
-            new PropertyValueFactory<Song,String>("artist")
-        );
-        colYear.setCellValueFactory(
-            new PropertyValueFactory<Song,String>("year")
-        );
-        colAlbum.setCellValueFactory(
-            new PropertyValueFactory<Song,String>("album")
-        );
 
-        // Listener for Table Selections, when a row in the table is selected, the song fields are updated
+        // setting correct data types of the columns of TableView
+        colTitle.setCellValueFactory(new PropertyValueFactory<Song, String>("title"));
+        colArtist.setCellValueFactory(new PropertyValueFactory<Song, String>("artist"));
+
+        // Listener for Table Selections, when a row in the table is selected, the song
+        // fields are updated
         songTable.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
             if (newSelection != null) {
                 current = newSelection;
@@ -67,83 +63,94 @@ public class MainController{
             }
         });
     }
+    
+    // adds song to list
+    public void addSong(ActionEvent e) {
+        if (titleField.getText() != null && artistField.getText() != null) {
+            current = new Song(titleField.getText(), artistField.getText());
 
-    public void addSong(ActionEvent e){
-        if(titleField.getText() != null && artistField.getText() != null)
-        {
-            songHolder = new Song(titleField.getText(), artistField.getText());
-            
-            
-            if(albumField.getText() != null)
-            {
-                songHolder.setAlbum(albumField.getText());
+            if (albumField.getText() != null) {
+                current.setAlbum(albumField.getText());
             }
-            if(yearField.getText() != null)
-            {
-                if(isNum(yearField.getText()))
-                    songHolder.setYear(yearField.getText());
+            if (yearField.getText() != null) {
+                if (isNum(yearField.getText()))
+                current.setYear(yearField.getText());
                 else
-                    System.out.println("invalid year");//error
-            } 
+                    System.out.println("invalid year");// error
+            }
+        } else {
+            // error message : no title or artist
         }
-        else
-        {
-            //error message : no title or artist
-        }
-        
-        
-        
-        
-        songs.add(songHolder);
-        songTable.setItems(songs);
-        clearFields();
-        
-        Gson gson = new Gson();
-        String jsonText = gson.toJson(songs);
-        System.out.println(jsonText);
-    }
-    public void updateSong(ActionEvent e){
 
-        current.setTitle(titleField.getText());
-        current.setArtist(artistField.getText());
-        
-        if(isNum(yearField.getText()))
+        if (!isDuplicate(current)) {
+            songs.add(current);
+            songTable.setItems(songs);
+            clearFields();
+
+            Gson gson = new Gson();
+            String jsonText = gson.toJson(songs);
+            System.out.println(jsonText);
+        }
+    }
+    // updates currently selected song
+    public void updateSong(ActionEvent e) {
+
+        // updatedSong will hold the values in Artist and Title fields
+        Song updatedSong = new Song(titleField.getText(), artistField.getText());
+
+        // It is then checked if it is a duplicate, if not, it will proceed the update
+        if (!isDuplicate(updatedSong)) {
+
+            current.setTitle(titleField.getText());
+            current.setArtist(artistField.getText());
+
+            current.setAlbum(albumField.getText());
+
+            if (isNum(yearField.getText()))
             current.setYear(yearField.getText());
-        else
-            System.out.println("invalid year");
-        
-        current.setAlbum(albumField.getText());
-        songTable.setItems(songs);
+            else
+                System.out.println("invalid year");
+
+            songTable.setItems(songs);
+            
+        }
         songTable.refresh();
     }
 
-    public void removeSong(ActionEvent e){
+    // removes song from list
+    public void removeSong(ActionEvent e) {
         songs.remove(current);
         songTable.setItems(songs);
         songTable.refresh();
         clearFields();
     }
 
-    private void clearFields(){
+    // clears text fields
+    private void clearFields() {
         titleField.clear();
         artistField.clear();
         yearField.clear();
         albumField.clear();
     }
 
-    private boolean isNum(String str)
-    {   
-        try  
-        {  
-            Integer.parseInt(str);      //testing if number
+    // Checks if argument is a number
+    private boolean isNum(String str) {
+        try {
+            Integer.parseInt(str);  // testing if number
             return true;
-        }  
-        catch(NumberFormatException nfe)  
-        {  
-            // invalid year/num
-            return false;
-        }  
+        } catch (NumberFormatException nfe) {
+            return false;           // invalid number (or year in this case)
+        }
     }
 
-   
+    // checks if a song is a duplicate of another song currently in the list
+    private boolean isDuplicate(Song check) {
+        for (Song s : songs) {
+            if (s.getTitle().equalsIgnoreCase(check.getTitle()))
+                if (s.getArtist().equalsIgnoreCase(check.getArtist()))
+                    return true;
+        }
+        return false;
+    }
+
 }
